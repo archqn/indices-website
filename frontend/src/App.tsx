c2d96284e52a8dd421a15b882de6757c26be347c
lucasjia <lucasjia@ucsb.edu>
2024-08-30 13:48:18 -0700

    fix small issue with tailwind and plotly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>AE Index Plot (Test)</h1>
+        <h1 className="text-blue-600" >AE Index Plot (Test)</h1>
       </header>
       <main>
         <TestPlot />
